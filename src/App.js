fefe6ba3e04fbd0aef88b6269ffeba194e239b11
jzhong16 <jooleyaz@gmail.com>
2024-01-03 08:21:07 -0800

    First test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function GradeRow() {
+  return (
+    <div class="row">
+      <div class="col">
+        <label class="col-label" for="col-grade">Grade</label>
+        <input type="text" class="col-input" id="col-grade"></input>
+      </div>
+      <div class="col">
+        <label class="col-label" for="col-credits">Credits</label>
+        <input type="text" class="col-input" id="col-credits"></input>
+      </div>
+    </div>
+  );
+}
+
+function AddButton() {
+  const [count, setCount] = useState(0);
+
+  function addRow() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button class="addButton" onClick={addRow}>
+      Add grade number {count + 1}.
+    </button>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Grade Calculator: UBC</h1>
+      <p>Please enter your current CPSC grades and credits:</p>
+      <GradeRow/>
+      <AddButton/>
     </div>
   );
 }
